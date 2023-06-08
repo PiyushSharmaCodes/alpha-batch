32b44ad8017783b0f9c5e4395d7fa3c5a95336b3
Piyush Sharma <piyushcodes123@gmail.com>
2023-06-08 12:32:10 +0530

    Code for sum of natural numbers upto given number is added

---
+++ Loops/SumOfNNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumOfNNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to which you want to print number");
+        int n= sc.nextInt();
+        int sum=0;
+        for (int i = 0; i <=n; i++) {
+            sum=sum+i;
+        }
+        System.out.println("The sum of first "+ n + "natural number is "+sum);
+    }
+}
