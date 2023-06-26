49b037fbdd6b29f056ecbe9acb20450f13037d00
Piyush Sharma <piyushcodes123@gmail.com>
2023-06-27 00:09:57 +0530

    Code for Prime Number Check is modified

--- Functions/PrimeOrNot.java
+++ Functions/PrimeOrNot.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class PrimeOrNot {
     public static boolean primecheck(int n) {
-        boolean prime=true;
+        boolean isprime=true;
         for(int i = 2 ; i <= Math.sqrt(n) ;i++){
             if (n%i==0) {
-                prime = false;
+                return false;
             }
         }
-        return prime;
+        return isprime;
     }
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
