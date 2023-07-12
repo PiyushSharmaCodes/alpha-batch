9f580e0d80929664a10b95748a3d81473802dd28
Piyush Sharma <piyushcodes123@gmail.com>
2023-07-12 23:25:25 +0530

    Code for Substring generation is added

--- String/StringCompare.java
+++ String/StringCompare.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class StringCompare {
+    public static String SubString(String s,int start , int  end) {
+        String substring="";
+        for (int i = start; i < end; i++) {
+            substring+=s.charAt(i);
+        }
+        return substring;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str1 ="hello";
@@ -23,6 +30,14 @@ public class StringCompare {
         }else{
             System.out.println("String 1 and 3 are not equal: using .equals() method");
         }
+        System.out.println("Enter start for substring");
+        int s = sc.nextInt();
+        System.out.println("Enter end for substring");
+        int e = sc.nextInt();
+        // System.out.println(SubString(str3, s, e));
+        // alternative to this function code is inbuit java method for substring
+        System.out.println(str3.substring(s, e));
+
         sc.close();
     }
 }
