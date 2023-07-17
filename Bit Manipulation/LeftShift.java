d4d4b38f260bb48da4ce49e93f232cdbd4469d3f
Piyush Sharma <piyushcodes123@gmail.com>
2023-07-17 23:26:19 +0530

    Code bitwise LeftShift is added

---
+++ Bit Manipulation/LeftShift.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class LeftShift {
+public static void main(String[] args) {
+    System.out.println("Enter a Number: ");
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    System.out.println("How many bits you have to shift left: ");
+    int no_bit=sc.nextInt();
+    System.out.println("Result of "+ num + "shifted "+ no_bit + " bits left is "+(num<<no_bit));
+    sc.close();
+}
+    
+}
