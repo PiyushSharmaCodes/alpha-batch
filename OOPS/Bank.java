c4ca9ce394a44300eee41180ee87c51c1667e0f8
Piyush Sharma <piyushcodes123@gmail.com>
2023-07-27 23:52:24 +0530

    Default and Parameterised constructor code is added

--- OOPS/Bank.java
+++ OOPS/Bank.java
@@ -2,13 +2,25 @@ import java.util.Scanner;
 
 public class Bank {
 public static void main(String[] args) {
-    BankAccount A1= new BankAccount();
-    System.out.println("Enter Username: ");
     Scanner sc = new Scanner(System.in);
-    A1.username=sc.next();
+    while (true) {
+        // BankAccount A1= new BankAccount();
+    System.out.println("Enter Username: ");
+    String username=sc.next();
     System.out.println("Enter Password: ");
-    A1.setPassword(sc.next());
+    String password = sc.next();
+    BankAccount A1 = new BankAccount(username, password);
     System.out.println("Paasword of "+A1.username+" is "+A1.getPassword());
+    System.out.println("Do you want to exit from the Account Creation: press 0");
+    int response=sc.nextInt();
+    
+    if (response==0) {
+        break;
+    }else{
+        continue;
+    }
+
+    }
     sc.close();
     
 }
--- OOPS/BankAccount.java
+++ OOPS/BankAccount.java
@@ -1,10 +1,19 @@
 public class BankAccount {
     public String username;
     private String password;
+    BankAccount(){
+        //default constructor or Non Parameterized
+    }
+    BankAccount(String username,String password){
+        //parameterized Constructor
+        this.username=username;
+        this.password=password;
+    }
     public void setPassword(String password) {
         this.password = password;
     }
     public String getPassword() {
         return password;
     }
+    
 }
