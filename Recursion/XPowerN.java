f891e1df136eaa0dfe59caea35af3275d0d9c3ca
Piyush Sharma <piyushcodes123@gmail.com>
2023-08-11 00:07:33 +0530

    code to find x to the power n(Optimised version) is  added

--- Recursion/XPowerN.java
+++ Recursion/XPowerN.java
@@ -7,13 +7,24 @@ public class XPowerN {
         }
         return n*FindPower(n, p-1);
     }
+    public static int FindPowerOptm(int n , int p) {
+        if (p==0) {
+            return 1;
+        }
+        int halfPower=FindPower(n,p/2);
+        int halfPowerSquare=halfPower*halfPower;
+        if (n%2!=0) {
+            halfPowerSquare*=n;
+        }
+        return halfPowerSquare;
+    }
     public static void main(String[] args) {
         System.out.println("Enter the Number :");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println("Enter Power: ");
         int p=sc.nextInt();
-        System.out.println(n+" to the power of "+p+" is "+ FindPower(n, p));
+        System.out.println(n+" to the power of "+p+" is "+ FindPowerOptm(n, p));
         sc.close();
     }
 }
